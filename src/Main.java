ba7d5fff65e31cb5e825f5f4af8e7a54148666c4
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-16 21:36:34 +0300

    Added realization of getting info using reflection.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,35 @@
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import model.entity.EquilateralTriangle;
 import model.util.annotation.Test;
 import model.util.test.SquareTest;
 
 public class Main {
 
     public static void main(String[] args) {
-	checkTests();
+	getInfo(EquilateralTriangle.class);
+
+	checkTests(SquareTest.class);
     }
 
-    private static void checkTests() {
-	Class test = SquareTest.class;
+    private static void getInfo(Class<?> clazz) {
+	System.out.println(clazz.getName());
 
+	Field[] fields = clazz.getDeclaredFields();
+	System.out.println(fields.length);
+	for (int i = 0; i < fields.length; i++) {
+	    System.out.println(fields[i]);
+	}
+
+	Annotation[] annotations = clazz.getAnnotations();
+	for (int i = 0; i < annotations.length; i++) {
+	    System.out.println(annotations[i].annotationType().getName());
+	}
+    }
+
+    private static void checkTests(Class test) {
 	Method[] methods = test.getMethods();
 	int pass = 0;
 	int fail = 0;
