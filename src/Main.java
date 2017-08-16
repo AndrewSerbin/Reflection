ec746b7ebc2f9feabfae032e9cb45863c0d2a6b8
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-16 20:40:13 +0300

    Added test reflection.

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Method;
+
+import model.util.annotation.Test;
+import model.util.test.SquareTest;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Class test = SquareTest.class;
+
+	Method[] methods = test.getMethods();
+	int pass = 0;
+	int fail = 0;
+
+	for (Method method : methods) {
+	    if (method.isAnnotationPresent(Test.class)) {
+		try {
+		    method.invoke(test);
+		} catch (Throwable e) {
+		    if (method.getAnnotation(Test.class).expected().equals(e.getClass())) {
+			pass++;
+		    } else {
+			fail++;
+		    }
+		}
+	    }
+	}
+
+	System.out.println("pass:" + pass + " fail:" + fail);
+    }
+}
